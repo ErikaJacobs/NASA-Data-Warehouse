85907c070b77925f48548783cbdd67fe21e1250b
erikajacobs <ErikaAshley3@gmail.com>
2020-05-27 22:12:06 -0700

    Update redshift.py

--- redshift.py
+++ redshift.py
@@ -4,17 +4,20 @@ import requests
 
 api_key = api_key
 
-def fetchAsteroidNeowsFeed():
+def neofeed():
   url = "https://api.nasa.gov/neo/rest/v1/feed"
   params = {
       'api_key':api_key,
       'start_date':'2020-01-22',
-      'end_date':'2020-01-23'
+      'end_date':'2020-01-22'
   }
   response = requests.get(url,params=params).json()
-  print(response)
+  #print(response.status_code)
   
-fetchAsteroidNeowsFeed()
+  print(response['near_earth_objects']['2020-01-22'][1]['links'])
+  #print(response)
+  
+neofeed()
 
 
 #%%
